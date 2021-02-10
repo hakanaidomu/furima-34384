943b0ec80abc02d233be8aba06ca91eb506dfdb0
sasaki <hakanaidomu@gmail.com>
2021-02-10 13:44:39 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -6,4 +7,27 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :delivery_day
+
+  with_options presence: true do
+    validates :title
+    validates :image
+    validates :about
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :delivery_day_id
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 99999999, message: "is invalid"}
+end
+
+
+  with_options numericality: { other_than: 1} do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
+  end
+  end
 end
