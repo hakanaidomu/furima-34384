a95d8f8ac552c0f1973af0794fddc1419fe48fdd
sasaki <hakanaidomu@gmail.com>
2021-02-10 19:26:15 +0900

    テストコードの記述及びバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,8 @@ class Item < ApplicationRecord
     validates :delivery_fee_id
     validates :prefecture_id
     validates :delivery_day_id
-    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 99999999, message: "is invalid"}
+    validates :price, numericality: { less_than_or_equal_to: 9999999}
+    validates :price, numericality: { greater_than_or_equal_to: 300}
 end
 
 
