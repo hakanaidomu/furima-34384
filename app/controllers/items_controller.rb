9654fee9a88ab16587b5c1afe764f93d897303e7
sasaki <hakanaidomu@gmail.com>
2021-02-11 17:00:12 +0900

    orderメソッドを用いて商品の出品順になるように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def new
-    @item = Item.new
+    @item = Item.new.order(created_at: :desc)
   end
 
   def create
