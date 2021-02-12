d59ba1a03fd536fe38ba0902e5c2d53ebfc7fb27
sasaki <hakanaidomu@gmail.com>
2021-02-12 18:27:33 +0900

    データをテーブルに保存する処理の記述

--- app/models/order_profile.rb
+++ app/models/order_profile.rb
@@ -11,5 +11,10 @@ class OrderProfile
   end
   validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
 
+
+  def save
+    order = Order.create(item: item, user_id: user_id)
+    Profile.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, tel: tel, order: order.id)
+  end
 end
 
