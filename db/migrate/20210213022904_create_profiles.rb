39affbce2fa01fb8ff31388fbbec63d0f9fc2bef
sasaki <hakanaidomu@gmail.com>
2021-02-13 11:37:19 +0900

    order,profileモデルのマイグレーションファイルの記述、及びマイグレーションの実行

--- db/migrate/20210213022904_create_profiles.rb
+++ db/migrate/20210213022904_create_profiles.rb
@@ -1,7 +1,13 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-
+      t.integer    :postal_code,     null: false
+      t.integer    :prefecture,      null: false
+      t.string     :city,            null: false         
+      t.string     :house_number,    null: false
+      t.string     :building_name
+      t.string     :tel,             null: false
+      t.references :order,           foreign_key: true
       t.timestamps
     end
   end
