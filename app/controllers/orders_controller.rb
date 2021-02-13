e9d9ca7101f50685e26afb8d67ecfa081ca49d76
sasaki <hakanaidomu@gmail.com>
2021-02-13 19:56:43 +0900

    @orderから@order_profileに記述を変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,10 @@ before_action :set_item, only: [:index, :create]
   end
 
   def create
-    @order = OrderProfile.new(order_params)
-    if @order.valid?
-      @order.sava
+    # binding.pry
+    @order_profile = OrderProfile.new(order_params)
+    if @order_profile.valid?
+      @orderprofile.sava
       redirect_to root_path
     else
       render :index
@@ -20,11 +21,9 @@ end
 private
 
 def order_params
-  params.require(:order_profile).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, 
-    :tel, :item_id).merge(user_id: current_user.id)
+  params.require(:order_profile).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :tel, :item_id).merge(user_id: current_user.id)
 end
 
 def set_item
   @item = Item.find(params[:item_id])
-  @order = OrderProfile.new
 end
