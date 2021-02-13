404f476824e8336107e3ea69bcfd1b1295fa7c30
sasaki <hakanaidomu@gmail.com>
2021-02-13 13:21:34 +0900

    orderコントローラーに set_itemを記述してindexを通る前に@itemを作る

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
-
+before_action :set_item, only: [:index]
   def index
     @order_profile = OrderProfile.new
   end
+end
+
+def set_item
+  @item = Item.find(params[:item_id])
 end
