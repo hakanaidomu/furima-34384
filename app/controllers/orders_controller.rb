5ecee07833fbe8b854287967604fe1da5cfc9acb
sasaki <hakanaidomu@gmail.com>
2021-02-14 22:12:20 +0900

    ログインしていないユーザーは購入ページにいけないようにbefore_actionを用いて実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
-before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order_profile = OrderProfile.new
